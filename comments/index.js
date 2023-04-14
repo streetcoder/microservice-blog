19758e1bb0f050fa75ec0c5d28404e9e5ab2a8e0
streetcoder <streetkoder@gmail.com>
2023-04-14 11:33:18 -0600

    fixed the error post and comments emitting

--- comments/index.js
+++ comments/index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
-const cors = require('cors');
-const axios = require('axios')
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
-app.use(cors())
+app.use(cors());
 
 const commentsByPostId = {};
 
@@ -21,18 +21,18 @@ app.post("/posts/:id/comments", async (req, res) => {
   comments.push({ id: commentId, content });
   commentsByPostId[req.params.id] = comments;
 
-  await axios.post('http://localhost:4005/events', {
-    type: 'CommentCreated',
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
     data: {
       id: commentId,
       content,
-      postId: req.params.id
-    }
-  })
+      postId: req.params.id,
+    },
+  });
   res.status(201).send(comments);
 });
 
-app.post("/events", (res, req) => {
+app.post("/events", (req, res) => {
   console.log("Received Event", req.body.type);
   res.send({});
 });
